607bfd41f84e9cd9a9bb8dc062047a4fd2a1e42e
minimindy <88155834+Minimindy@users.noreply.github.com>
2023-11-21 11:17:44 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,32 +1,50 @@
 from llama_cpp import Llama 
+from argparse import ArgumentParser
 import timeit
 
-# Load Llama 2 model
-llm = Llama(model_path="./model/llama-2-7b-chat.Q2_K.gguf",
-            n_ctx=512,
-            n_batch=128)
+def main():
+    parser = ArgumentParser()
+    
+    # Add the model path as a command-line argument
+    parser.add_argument("-m", "--model_path", help="Path to the model",
+                         dest="model_path", required=True)
+    
+    # Parse the command-line arguments
+    args = parser.parse_args()
 
-# Start timer
-start = timeit.default_timer()
+    # Load Llama 2 model
+    llm = Llama(model_path=args.model_path,
+                n_ctx=512,
+                n_batch=128)
 
-# Generate LLM response
-prompt = "Who wrote Romeo and Juliet?"
+    # Start timer
+    start = timeit.default_timer()
 
-output = llm(prompt,
-             max_tokens=100,
-             echo=False,
-             temperature=0.1,
-             top_p=0.9)
+    # Generate LLM response
+    prompt = "問題:誰是莎士比亞"
 
-# Stop timer
-stop = timeit.default_timer()
-duration = stop - start
-print("Time: ", duration, '\n\n')
+    output = llm(prompt,
+                 max_tokens=-1,
+                 echo=True,
+                 top_p=0.9)
 
-# Display generated text
-print(output['choices'][0]['text'])
+    # Stop timer
+    stop = timeit.default_timer()
+    duration = stop - start
+    print("\nTime: ", duration, '\n\n')
 
-# Write to file
-with open("response.txt", "w") as f:
-  f.write(f"Time: {duration}")
-  f.write(output['choices'][0]['text'])
+    # Display generated text
+    print(output['choices'][0]['text'])
+
+    # Write to file
+    write_txt(args.model_path, prompt, duration, output)
+
+def write_txt(model_path, prompt, duration, output):
+    with open("response.txt", "a", encoding="utf-8") as f:
+        f.write(f"{model_path}\n")
+        f.write(f"Prompt: {prompt}\n")  
+        f.write(f"Time: {duration}\n")  
+        f.write(f"Output: {output['choices'][0]['text']}\n\n")  
+
+if __name__ == '__main__':
+    main()
